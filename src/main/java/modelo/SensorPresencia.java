67b0f6c7d7045b674903ecdbc22dcf5587892575
Iulian <iga0001@alu.utamed.es>
2026-05-27 14:39:01 +0200

    Se añade el modelo para el sensor de presencia

---
+++ src/main/java/modelo/SensorPresencia.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class SensorPresencia extends Sensor {
+
+    public SensorPresencia() {
+        super("PIR-001", "Sensor de Presencia", "");
+    }
+
+    @Override
+    public void actualizarValor() {
+        valorActual = random.nextInt(1);
+    }
+
+    @Override
+    public String getEstadoActual() {
+        return valorActual == 1 ? "Detectado" : "No detectado";
+    }
+}
