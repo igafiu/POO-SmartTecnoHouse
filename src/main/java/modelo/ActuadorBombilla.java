cb780885e4d5058a18f565aabe426e66a23eab2c
Iulian <iga0001@alu.utamed.es>
2026-05-27 14:52:44 +0200

    Se crea el modelo para el actuador de bombillas

---
+++ src/main/java/modelo/ActuadorBombilla.java
@@ -0,0 +1,20 @@
+package modelo;
+
+public class ActuadorBombilla extends Actuador {
+
+    private static final String[] ACCIONES = {"ON", "OFF"};
+
+    public ActuadorBombilla() {
+        super("bulb", "Bombilla", "OFF");
+    }
+
+    @Override
+    public void ejecutarAccion(String accion) {
+        throw new IllegalArgumentException("Acción no válida para Bombilla: " + accion);
+    }
+
+    @Override
+    public String[] getAccionesPosibles() {
+        return ACCIONES;
+    }
+}
