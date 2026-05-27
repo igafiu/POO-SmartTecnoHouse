1b85ccf546823875b0b97111fab98bd21066cda5
Iulian <iga0001@alu.utamed.es>
2026-05-27 17:59:31 +0200

    Prueba conceptual tablas

--- src/main/java/vista/PanelSensores.java
+++ src/main/java/vista/PanelSensores.java
@@ -1,10 +1,26 @@
 package vista;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 
 public class PanelSensores extends JPanel {
 
     public PanelSensores() {
 
+        String[] columnas = {"ID", "Nombre", "Valor actual"};
+        DefaultTableModel modeloTabla = new DefaultTableModel(columnas, 0) {
+            @Override
+            public boolean isCellEditable(int row, int col) {
+                return false;
+            }
+        };
+
+        JTable tabla = new JTable(modeloTabla);
+        tabla.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tabla.getTableHeader().setReorderingAllowed(false);
+        tabla.setRowHeight(24);
+
+        add(new JScrollPane(tabla), BorderLayout.CENTER);
     }
 }
