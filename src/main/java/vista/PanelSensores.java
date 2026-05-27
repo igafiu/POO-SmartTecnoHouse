9f91e27fb2914c50739d4ba2d4ae0f10a65768aa
Iulian <iga0001@alu.utamed.es>
2026-05-27 17:39:25 +0200

    Prueba conceptual pestañas

---
+++ src/main/java/vista/PanelSensores.java
@@ -0,0 +1,10 @@
+package vista;
+
+import javax.swing.*;
+
+public class PanelSensores extends JPanel {
+
+    public PanelSensores() {
+
+    }
+}
--- src/main/java/vista/VentanaPrincipal.java
+++ src/main/java/vista/VentanaPrincipal.java
@@ -4,9 +4,16 @@ import javax.swing.*;
 
 public class VentanaPrincipal extends JFrame {
 
+    private final PanelSensores panelSensores;
+
     public VentanaPrincipal() {
         super("SmartTecnoHouse");
         setSize(900, 620);
         setLocationRelativeTo(null);
+
+        panelSensores = new PanelSensores();
+        JTabbedPane pestanas = new JTabbedPane();
+        pestanas.addTab("Sensores", panelSensores);
+        add(pestanas);
     }
 }
